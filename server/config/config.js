53ee77c3e669b796aca61a2ef002b6d52bce7bc6
Richard Antao <rantao@uwo.ca>
2019-08-03 22:42:56 -0400

    Changed env configurations

--- server/config/config.js
+++ server/config/config.js
@@ -1,17 +1,19 @@
 const Sequelize = require("sequelize");
-const name = process.env.DB_NAME; 
-const user = process.env.DB_USER; 
-const password = process.env.DB_PASSWORD; 
-const host = process.env.DB_HOST; 
+const env = process.env.NODE_ENV;
+const db = {
+  name: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST
+}
 
-
-const sequelize = new Sequelize(name, user, password, {
-	host: host,
+const sequelize = new Sequelize(db.name, db.user, db.password, {
+	host: db.host,
 	dialect: "mysql"
 });
 
 sequelize.authenticate().then(function() {
-    console.log('The Sequelize connection has been established successfully.');
+    console.log(`The Sequelize connection to the ${env} database has successfully authenticated`);
   }).catch(function(err) {
     console.error('Unable to connect to the database:', err);
   });

beef982ba540338fd714421d7cd507cf620c84dc
Richard Antao <richardmantao@gmail.com>
2019-08-04 19:54:52 -0400

    Added config folder

---
+++ server/config/config.js
@@ -0,0 +1,32 @@
+const Sequelize = require("sequelize");
+const env = process.env.NODE_ENV;
+const db = {
+  name: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST
+}
+
+if (env === "production") {
+
+} else if (env === "testing") {
+
+} else {
+  var sequelize = new Sequelize(db.name, db.user, db.password, {
+    host: db.host,
+    dialect: "mysql"
+  });
+
+  sequelize.authenticate().then(function() {
+      console.log(`The Sequelize connection to the ${env} database has successfully authenticated`);
+    }).catch(function(err) {
+      console.error('Unable to connect to the database:', err);
+    });
+
+  var config = {
+    sequelize: sequelize,
+    Sequelize: Sequelize
+  };
+}
+
+module.exports = config;

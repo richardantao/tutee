ec316e2c2e9d8ea6b0ca60969147c5e51cc9431d
Richard Antao <rantao@uwo.ca>
2019-08-03 22:26:26 -0400

    Initial import

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,12 @@
 const Sequelize = require("sequelize");
+const name = process.env.DB_NAME; 
+const user = process.env.DB_USER; 
+const password = process.env.DB_PASSWORD; 
+const host = process.env.DB_HOST; 
 
-const sequelize = new Sequelize("tutee", "root", "realpimp69!", {
-	host: "localhost",
+
+const sequelize = new Sequelize(name, user, password, {
+	host: host,
 	dialect: "mysql"
 });
 

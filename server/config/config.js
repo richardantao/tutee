69aad6a34ad7938d215b4681631a33020eb8f4e9
Richard Antao <richardmantao@gmail.com>
2019-08-08 18:41:56 -0400

    Updated configurations

--- server/config/config.js
+++ server/config/config.js
@@ -4,17 +4,25 @@ const db = {
   name: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST
+  host: process.env.DB_HOST,
+  url: process.env.DB_URL
 }
 
 if (env === "production") {
-
+  var sequelize = new Sequelize(url, {});
 } else if (env === "testing") {
-
+  var sequelize = new Sequelize(); // define testing database location
 } else {
   var sequelize = new Sequelize(db.name, db.user, db.password, {
     host: db.host,
-    dialect: "mysql"
+    dialect: "mysql",
+
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
   });
 
   sequelize.authenticate().then(function() {

6c91497b7e0538eed491df25ac896ab5656625c1
Richard Antao <richardmantao@gmail.com>
2019-08-07 20:21:27 -0400

    Added 404 page component

---
+++ client/src/components/pages/NotFound/NotFound.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import LoadingColumn from "../../molecules/LoadingColumn";
+import styles from "./NotFound.css";
+
+
+export default class NotFound extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoading: true
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            isLoading: false
+        });
+    }
+    
+    render() {
+        let { isLoading } = this.state;
+
+        if (isLoading) {
+            return LoadingColumn;
+        } else {
+            return (
+                <div>
+
+                </div>
+            )
+        }
+    }
+}

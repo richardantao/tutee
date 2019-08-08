c86b566e81ddddbff86c5c752552e3dc7311fad2
Richard Antao <richardmantao@gmail.com>
2019-08-08 18:43:00 -0400

    Updated iso

--- universal/universal.js
+++ universal/universal.js
@@ -7,4 +7,22 @@ import routes from "../client/src/index";
 export default function universal(req, res) {
     const branch = matchRoutes(routes, req.url);
     const promises = [];
+
+    branch.forEach(({route, match}) => {
+        if (route.loadData) {
+            promises.push(route.loadData(match));
+        }
+    });
+
+    Promise.all(promises).then(data => {
+        const context = data.reduce((context, data) => Object.assign(context, data), {});
+
+        const router = <StaticRouter location={req.url} context={context}><App /></StaticRouter>;
+
+        const app = renderToString(router);
+
+        const html = renderToString(<HTML html={app} />); // change to personal component
+
+        return res.send(`<!DOCTYPE html>${html}`);
+    });
 }

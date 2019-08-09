230f5796919ff58fb95d3d51f3962455cb73fa04
Richard Antao <richardmantao@gmail.com>
2019-08-09 12:57:13 -0400

    Updated components

--- client/src/components/organisms/Skeleton/Skeleton.js
+++ client/src/components/organisms/Skeleton/Skeleton.js
@@ -1,4 +1,20 @@
 import { createSkeletonProvider, createSkeletonElement } from '@trainline/react-skeletor';
 import styles from "./Skeleton.css";
 
-const skeletonNav createSkeletonElement("div");
+const navButton = createSkeletonElement("div");
+
+const sideBar = ({button}) => {
+    <div>
+        <navButton />
+        <navButton />
+        <navButton />
+        <navButton />
+        <br/> 
+        <navButton />  
+    </div>
+}
+
+export default createSkeletonProvider(
+
+)(sideBar)
+

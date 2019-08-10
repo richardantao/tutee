cf311b2f7c601360d3dc18027f11d15f609412cf
Richard Antao <richardmantao@gmail.com>
2019-08-10 11:24:29 -0400

    Updated controllers

--- server/controllers/courses.js
+++ server/controllers/courses.js
@@ -580,7 +580,7 @@ exports.modulesDelete = function(req, res) {
 	const errors = validationResult(req);
 		
 	if(!errors.isEmpty()) {
-		return res.status(422).json({ errors: errors.array() });
+		return res.status(400).json({ errors: errors.array() });
 	} else {
 		Modules.destroy({
 			where: { id: req.params.id }

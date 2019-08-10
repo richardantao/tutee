2cd38de486e9bb2f1c2d0e580571420cd84c8380
Richard Antao <richardmantao@gmail.com>
2019-08-09 20:51:53 -0400

    Updated app.js

--- server/app.js
+++ server/app.js
@@ -79,7 +79,7 @@ app.use(function(err, req, res, next) {
 });
 
 // server side render, view engine 
-app.get("/*", function(req, res) {
+app.get("*", function(req, res) {
 	res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 

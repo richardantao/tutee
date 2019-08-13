27980e8b99536f108caaa7ed4efe8e5390378721
Richard Antao <richardmantao@gmail.com>
2019-08-12 20:18:21 -0400

    Created template queries

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,45 +1,42 @@
-module.exports = (models, Sequelize) => {
-	const Users = models.define("Users", {
-		id: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			allowNull: false,
-			autoIncrement: true
-		},
-		isBetaMember: {
-			type: Sequelize.BOOLEAN,
-			defaultValue: false,
-			allowNull: false
-		},
-		firstName: {
-			type: Sequelize.STRING(25),
-			allowNull: false
-		},
-		lastName: {
-			type: Sequelize.STRING(25),
-			allowNull: false
-		},
-		password: {
-			type: Sequelize.STRING(25),
-			allowNull: false
-		},
-		passwordChange: {
-			type: Sequelize.STRING(25)
-		},
-		email: {
-			type: Sequelize.STRING(50),
-			allowNull: false
-		}, 
-		country: {
-			type: Sequelize.STRING(50)
-		}, 
-		region: {
-			type: Sequelize.STRING(50)
-		},
-		institution: {
-			type: Sequelize.STRING(100)		   
-		}
-	}, {});	
+// import database
+const database = require("../config/config");
 
-	return Users;
+// instantiate model
+const Users = [];
+
+// GET all users; admin query
+Users.findAll = () => {
+	return database.query(
+
+	);
 }
+
+// GET when user opens personal settings page
+Users.findById = () => {
+	return database.query(
+
+	);
+}
+
+// POST to db when user creates a new account
+Users.create = () => {
+	return database.query(
+
+	);
+}
+
+// PUT when user updates personal data in settings tab
+Users.update = () => {
+	return database.query(
+
+	);
+}
+
+// DELETE when user deletes account
+Users.delete = () => {
+	return database.query(
+
+	);
+}
+
+module.exports = Users;

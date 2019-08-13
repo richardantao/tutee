30b9a5ccc4bd4e02941f6e0cc3192eac751101ba
Richard Antao <richardmantao@gmail.com>
2019-08-12 21:10:56 -0400

    Input query statements

--- server/models/Years.js
+++ server/models/Years.js
@@ -10,27 +10,41 @@ Years.findAll = () => {
 	);
 }
 
-Years.findbyId = () => {
+Years.findbyId = (req) => {
+	let userId = req.params.UserId;
+
 	return database.query(
-		``
+		` SELECT * FROM Years
+		WHERE UserId = ${userId}`
 	);
 }
 
-Years.create = () => {
+Years.create = (req, res) => {
+	
 	return database.query(
-		``
+		`INSERT INTO Years 
+		(UserFirstName, UserLastName, UserEmail, UserPassword, UserCountry, UserRegion, UserInstitution) 
+		VALUES ()`
 	);
 }
 
 Years.update = () => {
+	let userId = req.params.UserId;
+	
+	let update = ``;
+
 	return database.query(
-		``
+		`UPDATE Years SET 
+		WHERE UserId = ${userId}`
 	);
 }
 
 Years.delete = () => {
+	let userId = req.params.UserId;
+	
 	return database.query(
-		``
+		`DELETE FROM Years
+		WHERE ${userId}`
 	);
 }
 

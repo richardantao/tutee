8a44521b9c72ccd41b78137c8f12aabdd91d32d9
Richard Antao <richardmantao@gmail.com>
2019-08-13 19:46:59 -0400

    Completed CRUD operations

--- server/models/Courses.js
+++ server/models/Courses.js
@@ -4,33 +4,68 @@ const database = require("../config/config");
 // instantiate model
 const Courses = [];
 
-Courses.findAll = () => {
+Courses.findAll = (req) => {
+	let userId = req.params.UserId;
+
 	return database.query(
-		``
+		`SELECT * FROM Courses
+		WHERE UserId = ${userId}`
 	);
 }
 
-Courses.findById = () => {
+Courses.findById = (req) => {
+	let userId = req.params.UserId;
+	let courseId = req.params.CourseId;
+
 	return database.query(
-		``
+		`SELECT * FROM Courses
+		WHERE UserId = ${userId} AND CourseId = ${courseId}`
 	);
 }
 
-Courses.create = () => {
+Courses.create = (req) => {
+	let userId = req.params.UserId;
+	let termId = req.params.TermId;
+	let created = {
+		courseCode: req.body.courseCode,
+		courseName: req.body.courseName,
+		courseTheme: req.body.courseTheme
+	}
+
 	return database.query(
-		``
+		`INSERT INTO Courses
+		(UserId, TermId, CourseCode, CourseName, CourseTheme)
+		VALUES (${userId}, ${termId}, ${created.courseCode}, 
+			${created.courseName}, ${created.courseTheme})`
 	);
 }
 
-Courses.update = () => {
+Courses.update = (req) => {
+	let userId = req.params.UserId;
+	let courseId = req.params.CourseId;
+	let updated = {
+		courseCode: req.body.courseCode,
+		courseName: req.body.courseName,
+		courseTheme: req.body.courseTheme
+	}
+
 	return database.query(
-		``
+		`UPDATE Courses
+		SET
+			CourseCode = ${updated.courseCode},
+			CourseName = ${updated.courseName},
+			CourseTheme ${updated.courseTheme}
+		WHERE UserId = ${userId} AND CourseId = ${courseId}`
 	);
 }
 
 Courses.delete = () => {
+	let userId = req.params.UserId;
+	let courseId = req.params.CourseId;
+
 	return database.query(
-		``
+		`DELETE FROM Courses
+		WHERE UserId = ${userId} AND CourseId = ${courseId}`
 	);
 }
 
--- server/models/Evaluations.js
+++ server/models/Evaluations.js
@@ -4,33 +4,85 @@ const database = require("../config/config");
 // instantiate model
 const Evals = [];
 
-Evals.findAll = () => {
+Evals.findAll = (req) => {
+	let userId = req.params.UserId;
+
 	return database.query(
-		``
+		`SELECT * FROM Evals
+		WHERE UserId = ${userId}`
 	);
 }
 
-Evals.findById = () => {
+Evals.findById = (req) => {
+	let userId = req.params.UserId;
+	let evalId = req.params.EvalId;
+
 	return database.query(
-		``
+		`SELECT * FROM Evals
+		WHERE UserId = ${userId} AND EvalId = ${evalId}`
 	);
 }
 
-Evals.create = () => {
+Evals.create = (req) => {
+	let userId = req.params.UserId;
+	let courseId = req.params.CourseId;
+	let created = {
+		evalTitle: req.body.evalTitle,
+		evalType: req.body.evalType,
+		evalLocation: req.body.evalLocation,
+		evalDate: req.body.evalDate,
+		evalTime: req.body.evalTime,
+		evalDuration: req.body.evalDuration,
+		evalWeighting: req.body.evalWeighting,
+		evalScore: req.body.Score
+	}
+
 	return database.query(
-		``
+		`INSERT INTO Evals
+		(UserId, CourseId, EvalTitle, EvalType, EvalLocation, EvalDate, EvalTime, EvalDuration, EvalWeighting, EvalScore)
+		VALUES (${userId}, ${courseId}, ${created.evalTitle}, 
+			${created.evalType}, ${created.evalLocation}, ${created.evalDate}, 
+			${created.evalTime}, ${created.evalDuration}, ${created.evalWeighting}, 
+			${created.evalScore})`
 	);
 }
 
-Evals.update = () => {
+Evals.update = (req) => {
+	let userId = req.params.UserId;
+	let evalId = req.params.EvalId;
+	let updated = {
+		evalTitle: req.body.evalTitle,
+		evalType: req.body.evalType,
+		evalLocation: req.body.evalLocation,
+		evalDate: req.body.evalDate,
+		evalTime: req.body.evalTime,
+		evalDuration: req.body.evalDuration,
+		evalWeighting: req.body.evalWeighting,
+		evalScore: req.body.Score
+	}
+
 	return database.query(
-		``
+		`UPDATE Evals
+		SET
+			EvalTitle = ${updated.evalTitle},
+			EvalType = ${updated.evalType},
+			EvalLocation = ${updated.evalLocation},
+			EvalDate = ${updated.evalDate},
+			EvalTime = ${updated.evalTime},
+			EvalDuration = ${updated.evalDuration},
+			EvalWeighting = ${updated.evalWeighting},
+			EvalSCore = ${updated.evalScore}
+		WHERE UserId = ${userId} AND EvalId = ${evalId}`
 	);
 }
 
-Evals.delete = () => {
+Evals.delete = (req) => {
+	let userId = req.params.UserId;
+	let evalId = req.params.EvalId;
+
 	return database.query(
-		``
+		`DELETE FROM Evals
+		WHERE UserId = ${userId} AND EvalId = ${evalId}`
 	);
 }
 

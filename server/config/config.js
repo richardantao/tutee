506a383ce9d06d96edefac5dd61d1df26e1d0073
Richard Antao <richardmantao@gmail.com>
2019-08-12 20:07:26 -0400

    Updated configurations

--- server/config/config.js
+++ server/config/config.js
@@ -1,65 +1,25 @@
-const Sequelize = require("sequelize");
-const env = process.env.NODE_ENV;
-
-const development = {
-  user: "",
-  password: "",
-  database: "",
-  host: "",
-  dialect: "mysql"
+const mysql = require("mysql");
+const env = {
+    env: process.env.NODE_ENV,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
 }
 
-const testing = {
-  user: "",
-  password: "",
-  database: "",
-  host: "",
-  dialect: "mysql"
-}
+const database = mysql.createConnection({
+  host: env.host,
+  database: env.database,
+  user: env.user,
+  password: env.password,
+});
 
-const production = {
-  user: "",
-  password: "",
-  database: "",
-  host: "",
-  dialect: "mysql"
-} 
+database.connect(function(err) {
+  if (err) {
+        throw err;
+  } else {
+        console.log(`Connected to Tutee's ${env.env} database`);
+  }
+});
 
-const db = {
-  name: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  url: process.env.DB_URL
-}
-
-if (env === "production") {
-  var sequelize = new Sequelize(url, {});
-} else if (env === "testing") {
-  var sequelize = new Sequelize(); // define testing database location
-} else {
-  var sequelize = new Sequelize(db.name, db.user, db.password, {
-    host: db.host,
-    dialect: "mysql",
-
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  });
-
-  sequelize.authenticate().then(function() {
-      console.log(`The Sequelize connection to the ${env} database has successfully authenticated`);
-    }).catch(function(err) {
-      console.error('Unable to connect to the database:', err);
-    });
-
-  var config = {
-    sequelize: sequelize,
-    Sequelize: Sequelize
-  };
-}
-
-module.exports = config;
+module.exports = database;

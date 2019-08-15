2e4282a221f12a11ee397e76e6c754c12d5b386e
Richard Antao <richardmantao@gmail.com>
2019-08-14 21:56:37 -0400

    Updated route

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,4 +2,6 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/users");
 
+router.get("/", controller.usersEdit);
+
 module.exports = router;

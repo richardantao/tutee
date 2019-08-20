362489aa88504a2d2c9a9c39a28c4e5c482661d6
Richard Antao <richardmantao@gmail.com>
2019-08-19 20:40:56 -0400

    Updated Settings tab

--- client/src/components/organisms/Profile/Profile.js
+++ client/src/components/organisms/Profile/Profile.js
@@ -1,10 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
+import { Col, Row} from "react-bootstrap"
+import ButtonReact from "../../atoms/Button";
+import "./Profile.scss";
 
-const Profile = () => {
-	return(
-		<div>
-		</div>
-	)
+export default class Profile extends Component {
+	constructor(props) {
+
+	}
+
+	componentDidMount() {
+		axios.get()
+		.then(res =>{
+
+		})
+		.catch(err => {
+
+		})
+	}
+	
+	render() {
+		return(
+			<form>
+				<Row>
+					<Col>
+						<label for="UserFirstName">First Name</label>
+					</Col>
+					<Col>
+						<input name="UserFirstName" type="text"/>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<label>Email</label>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+					
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+					
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<ButtonReact type="reset">Clear Changes</ButtonReact>
+						<ButtonReact type="submit">Save</ButtonReact>
+					</Col>
+				</Row>
+			</form>
+		)
+	}
 }
 
-export default Profile;

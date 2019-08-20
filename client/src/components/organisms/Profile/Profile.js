805f7e8f715c3d4fc74595c65b43e290682be344
Richard Antao <richardmantao@gmail.com>
2019-08-19 20:59:02 -0400

    Updated Settings/profile

--- client/src/components/organisms/Profile/Profile.js
+++ client/src/components/organisms/Profile/Profile.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Col, Row} from "react-bootstrap"
+import LoadingColumn from "../../molecules/LoadingColumn";
 import ButtonReact from "../../atoms/Button";
+import SelectReact from "../../atoms/Select";
 import "./Profile.scss";
 
 export default class Profile extends Component {
@@ -20,39 +22,53 @@ export default class Profile extends Component {
 	}
 	
 	render() {
-		return(
-			<form>
-				<Row>
-					<Col>
-						<label for="UserFirstName">First Name</label>
-					</Col>
-					<Col>
-						<input name="UserFirstName" type="text"/>
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-						<label>Email</label>
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-					
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-					
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-						<ButtonReact type="reset">Clear Changes</ButtonReact>
-						<ButtonReact type="submit">Save</ButtonReact>
-					</Col>
-				</Row>
-			</form>
-		)
+		let { isLoading } = this.state;
+		if (isLoading) {
+			return LoadingColumn;
+		} else {
+			return(
+				<form method="PUT" action="/:UserId/settings/profile/FILLME/update">
+					<Row>
+						<Col>
+							<label for="UserFirstName">First Name</label>
+							<input name="UserFirstName" type="text"/>
+						</Col>
+						<Col>
+							<label for="UserLastName">Last Name</label>
+							<input name="UserLastName" type="text"/>
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<label>Email</label>
+							<input type="email" name="UserEmail" value=""/>
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<label for="UserCountry">Country</label>
+							<input name="UserCountry" type="text"/>
+						</Col>
+						<Col>
+							<label for="UserRegion">Province/State</label>				
+							<input name="UserRegion" type="text"/>
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<label for="UserInstitution">Institution</label>
+							<SelectReact name="UserInstitution"/>
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<ButtonReact type="reset">Clear Changes</ButtonReact>
+							<ButtonReact type="submit">Save Changes</ButtonReact>
+						</Col>
+					</Row>
+				</form>
+			)
+		}
 	}
 }
 

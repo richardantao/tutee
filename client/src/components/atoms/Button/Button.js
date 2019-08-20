66b1ef69acc6c23cac38f551c651adf5a27c1b34
Richard Antao <richardmantao@gmail.com>
2019-08-20 17:41:46 -0400

    Resolved warnings

--- client/src/components/atoms/Button/Button.js
+++ client/src/components/atoms/Button/Button.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import styles from "./Button.scss";
+import "./Button.scss";
 
 const ButtonReact = (props) => (
 	<Button>{props.icon}{props.name}</Button>
--- client/src/index.js
+++ client/src/index.js
@@ -9,17 +9,9 @@ import {
 } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 
-import Dashboard from "./components/pages/Dashboard";
-import Calendar from "./components/pages/Calendar";
-import Tasks from "./components/pages/Tasks";
-import Evaluations from "./components/pages/Evaluations";
-import Courses from "./components/pages/Courses";
-import Settings from "./components/pages/Settings";
-import NotFound from "./components/pages/NotFound";
-
 const Index = () => <App />
 
-render (
+render(
 	<Router>
 		<Index />
 	</Router>,

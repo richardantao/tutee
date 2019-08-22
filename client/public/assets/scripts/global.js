120a21bde96d648f87ec40caee7513769a667610
Richard Antao <richardmantao@gmail.com>
2019-08-22 19:34:45 -0400

    Added copyright script

--- client/public/assets/scripts/global.js
+++ client/public/assets/scripts/global.js
@@ -1,5 +1,6 @@
-window.onload = function() {
-    let year = new Date.getFullYear();
+// render copyright statement inside footer after window loads 
+window.onload = function copyrightYear() {
+    var year = new Date().getFullYear();
 
     document.querySelector("#copyright").innerHTML = "Copyright <i class='far fa-copyright'></i> " + year + " Tutee LLC. All rights reserved.";
 }

466a00039d6812adc3256041e5fced9b14d34f83
Richard Antao <richardmantao@gmail.com>
2019-08-25 21:46:02 -0400

    Refactored imports

--- client/public/assets/scripts/global.js
+++ client/public/assets/scripts/global.js
@@ -1,6 +1,6 @@
 // render copyright statement inside footer after window loads 
 window.onload = function copyrightYear() {
-    var year = new Date().getFullYear();
+    let year = new Date().getFullYear();
 
     document.querySelector("#copyright p").innerHTML = "Copyright <i class='far fa-copyright'></i> " + year + " Tutee LLC. All rights reserved.";
 }

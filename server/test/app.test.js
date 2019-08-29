bc81624506eb4c544ac407163633a4c5f03a2670
Richard Antao <richardmantao@gmail.com>
2019-08-28 21:27:22 -0400

    Added statuscode 200 test

--- server/test/app.test.js
+++ server/test/app.test.js
@@ -1,2 +1,12 @@
+const should = require("chai").should();
+const expect = require("chai").expect;
 const assert = require("chai").assert;
+const request = require("request");
 const app = require("../app.js");
+
+it("Home page rendered",(done) => {
+    request("http://localhost:3001/home.html",(err, res, body) => {
+        expect(res.statusCode).to.equal(200);
+        done(); 
+    });
+});

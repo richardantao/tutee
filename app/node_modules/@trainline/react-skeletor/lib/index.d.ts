ca4e7f28c4e08bb4d74554de76a5e2d9d8388327
Richard Antao <richardmantao@gmail.com>
2019-08-28 21:38:29 -0400

    changed react directory name

---
+++ app/node_modules/@trainline/react-skeletor/lib/index.d.ts
@@ -0,0 +1,4 @@
+import { createSkeletonElement } from './createSkeletonElement';
+export { createSkeletonProvider } from './createSkeletonProvider';
+export { createSkeletonElement } from './createSkeletonElement';
+export default createSkeletonElement;

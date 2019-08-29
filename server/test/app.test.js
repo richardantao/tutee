5bbf31e61f3ff368a8b4fb87ef3d31ca6a7825a0
Richard Antao <richardmantao@gmail.com>
2019-08-28 21:29:26 -0400

    Added tests

--- server/test/app.test.js
+++ server/test/app.test.js
@@ -4,9 +4,16 @@ const assert = require("chai").assert;
 const request = require("request");
 const app = require("../app.js");
 
-it("Home page rendered",(done) => {
+it("Home page rendered OK", (done) => {
     request("http://localhost:3001/home.html",(err, res, body) => {
         expect(res.statusCode).to.equal(200);
         done(); 
+    });
+});
+
+it("", (done) => {
+    request("http://localhost:3001/", (err, res, body) => {
+        expect();
+        done();
     });
 });

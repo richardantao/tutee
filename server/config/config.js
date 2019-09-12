923a7ae634b20eb93b4c8ba501138ab8ad883a71
Richard Antao <richardmantao@gmail.com>
2019-09-11 23:13:44 -0400

    Basic config file for mongo db

--- server/config/config.js
+++ server/config/config.js
@@ -1,35 +1,12 @@
-// import database client
 const MongoClient = require("mongodb").MongoClient;
-const url = "mongodb://localhost:27017/mydb";
-
-// import environment variables
-const env = {
-  env: process.env.NODE_ENV,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD
-}
-
-db.createCollection('test', (err, collection) => {
-  
-});
-
-
+const url = "mongo://localhost:27017/TuteeDev";
 
 MongoClient.connect(url, (err, db) => {
   if (err) {
-   throw err; 
-  }
-  else {
-    const database = db.db("TuteeDB");
-    database.createCollection("test", err => {
-      if (err) {
-        throw err;
-      } else {
-        console.log("Collection created!");
-        db.close();
-      }
-    });
+    throw err;
+  } else {
+    console.log("");
+
+    db.close();
   }
 });

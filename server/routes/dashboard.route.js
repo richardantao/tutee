c2cc6f6cd1eacc13d05db43a0c785bb038a485f0
Richard Antao <richardmantao@gmail.com>
2019-09-14 15:08:54 -0400

    Changed routes

--- server/routes/dashboard.route.js
+++ server/routes/dashboard.route.js
@@ -3,34 +3,34 @@ const router = express.Router();
 const controller = require("../controllers/dashboard.controller");
 
 // GET specific class selected by user
-router.get("classes/:classId/edit", controller.dashboardClassEdit); // routed in react
+router.get("classes/:classId/edit", controller.classEdit); // routed in react
 
 // DELETE specific class selected by user
-router.delete("/classes/:classId/delete", controller.dashboardClassDelete); 
+router.delete("/classes/:classId/delete", controller.classDelete); 
 
 // GET specific task selected by user
-router.get("/tasks/:taskId/edit", controller.dashboardTaskEdit); // routed in react
+router.get("/tasks/:taskId/edit", controller.taskEdit); // routed in react
 
 // GET data for user to POST new tasks
-router.get("/tasks/create", controller.dashboardTaskCreateGet); // routed in react
+router.get("/tasks/create", controller.taskCreateGet); // routed in react
 
 // POST new task created by user
-router.post("/tasks/create", controller.dashboardTaskCreatePost); // routed in react
+router.post("/tasks/create", controller.taskCreatePost); // routed in react
 
 // PUT task selected by user
-router.put("/tasks/:TaskId/update", controller.dashboardTaskUpdate);
+router.put("/tasks/:TaskId/update", controller.taskUpdate);
 
 // DELETE task selected by user
-router.delete("/tasks/:TaskId/delete", controller.dashboardTaskDelete);
+router.delete("/tasks/:TaskId/delete", controller.taskDelete);
 
 // GET specific eval selected by user
-router.get("/evals/:EvalId/edit", controller.dashboardEvaluEdit); // routed in react
+router.get("/evals/:EvalId/edit", controller.evalEdit); // routed in react
 
 // PUT specific eval selected by user
-router.put("/evals/:EvalId/update", controller.dashboardEvaluUpdate);
+router.put("/evals/:EvalId/update", controller.evalUpdate);
 
 // DELETE specific eval selected by user
-router.delete("/evals/:EvalId/delete", controller.dashboardEvaluDelete);
+router.delete("/evals/:EvalId/delete", controller.evalDelete);
 
 module.exports = router;
 

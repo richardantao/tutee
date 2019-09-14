ac779178666211370182c02b9c4b4231a60abad6
Richard Antao <richardmantao@gmail.com>
2019-09-13 20:48:55 -0400

    Updated model

--- server/models/Users.js
+++ server/models/Users.js
@@ -20,15 +20,15 @@ const UserSchema = new Schema({
         school: String
     },
     preferences: {
-        startDay: String,
+        startDay: {type: String, enum: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]},
         startTime: {type: String, default: "8:00am"},
         defaultDuration: {type: Number, default: 50, min: [0, "Default duration must be greater than 0"]},
-        defaultCalendar: {type: String, default: "Week"},
+        defaultCalendar: {type: String, default: "Week", enum: ["Month", "Week", "Day", "Agenda"]},
         onEmailList: {type: Boolean, default: true}
     },
     meta: {
         isBeta: Boolean,
-        membership: {type: String, default: "Basic"},
+        membership: {type: String, default: "Basic", enum: ["Basic, Premium, Beta"]},
         sessions: {type: Number, default: 0, min: 0},
         lastLogin: Date,
         createdAt: {type: Date, default: Date.now()},

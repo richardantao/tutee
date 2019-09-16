a46a4d8b67d5587d6a18ba77443a580bf3307401
Richard Antao <richardmantao@gmail.com>
2019-09-16 01:30:51 -0400

    Added new controllers

---
+++ server/controllers/sessions.controller.js
@@ -0,0 +1,15 @@
+// import model to check
+const Users = require("../models/Users.model");
+
+// instantiate 
+const controller = [];
+
+controller.login = (req, res) => {
+    
+}
+
+controller.logout = (req, res) => {
+    
+}
+
+module.exports = controller;

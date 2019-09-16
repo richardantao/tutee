b1c44fedbe70c4d48d036ff75c383c83d40d1ca8
Richard Antao <richardmantao@gmail.com>
2019-09-16 17:47:43 -0400

    Added model

--- server/models/Classes.model.js
+++ server/models/Classes.model.js
@@ -5,6 +5,16 @@ const moment = require("moment");
 
 const ClassSchema = new Schema({
     id: Schema.Types.ObjectId,
+    module: {type: String, required: true},
+    date: {
+        start: {type: Date, required: true},
+        end: {type: Date, required: true}
+    },
+    time: {
+        start: {type: String, required: true},
+        end: {type: String, required: true}
+    },
+    location: String,
     meta: {
 	    createdAt: {type: Date, default: Date.now()},
         updatedAt: {type: Date, default: Date.now()}

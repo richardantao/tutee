95cb440e362a487d3582e4ec943b2e8221b3add5
Richard Antao <richardmantao@gmail.com>
2019-09-18 15:58:43 -0400

    Updated controller

--- server/controllers/email.controller.js
+++ server/controllers/email.controller.js
@@ -5,7 +5,15 @@ const Email = require("../models/Email.model");
 const controller = [];
 
 controller.beta = (req, res) => {
-
+    Email.betaReq()
+    .then(email => {    
+        return res.redirect(301, "https:localhost:3000/home.html");
+    })
+    .catch(err => {
+        return res.status(500).json({
+            message: err.message || "An error occured while submitting this email"
+        });
+    });
 }
 
 controller.contact = (req, res) => {
--- server/controllers/sessions.controller.js
+++ server/controllers/sessions.controller.js
@@ -1,7 +1,7 @@
 // import model to check
 const Users = require("../models/Users.model");
 
-// instantiate 
+// instantiate model
 const controller = [];
 
 controller.login = (req, res) => {

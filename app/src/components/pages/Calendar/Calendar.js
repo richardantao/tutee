da040dca997de03f89c6f801c4e00b043f346081
Richard Antao <richardmantao@gmail.com>
2019-09-25 14:09:17 -0400

    Updated Calendar component

--- app/src/components/pages/Calendar/Calendar.js
+++ app/src/components/pages/Calendar/Calendar.js
@@ -1,10 +1,12 @@
 import React, { Component, Fragment } from "react";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import Nav from "../../organisms/Nav";
 import Header from "../../organisms/Header";
 import { Container, Row, Col} from "reactstrap";
 import { Button } from "react-bootstrap"
 import "./Calendar.scss";
-import axios from "axios";
 
 export default class Calendar extends Component {
 	constructor(props) {
@@ -20,9 +22,12 @@ export default class Calendar extends Component {
 			isLoading: false
 		});
 
-		axios({
-			method: "GET",
-			url: ""
+		axios.get("http://localhost:3000/calendar/")
+		.then(res => {
+
+		})
+		.catch(err => {
+
 		});
 	}
 
@@ -35,12 +40,23 @@ export default class Calendar extends Component {
 						<Col>
 							<Header header="Calendar"/> 
 						</Col>
-						<Col>
-						
+						<Col className="header-date">
+							<Button onClick="">
+								<FontAwesomeIcon icon={faChevronLeft}/>
+							</Button>
+							<h4>{this.props.date}</h4>
+							<Button onClick="">
+								<FontAwesomeIcon icon={faChevronRight}/>
+							</Button>
+						</Col>
+						<Col className="calendar-selector">
+							<Button>{this.props.display}</Button>
 						</Col>
 					</Row>
 					<Row className="body">
+						<Col>
 						
+						</Col>
 					</Row>
 				</Container>
 			</Fragment>

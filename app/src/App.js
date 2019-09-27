aa1d6d5c6d8f4e5ec78cefce00e43a0c06d34229
Richard Antao <richardmantao@gmail.com>
2019-09-26 23:29:55 -0400

    Updated routes

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
-import './App.scss';
-import "./App";
-
 import Dashboard from "./components/pages/Dashboard";
 import Calendar from "./components/pages/Calendar";
 import Tasks from "./components/pages/Tasks";
@@ -10,11 +7,13 @@ import Evaluations from "./components/pages/Evaluations";
 import Courses from "./components/pages/Courses";
 import Search from "./components/pages/Search";
 import Settings from "./components/pages/Settings";
-	import Profile from "./components/organisms/Profile";
-	import Password from "./components/organisms/Password";
-	import Preference from "./components/organisms/Preference";
-	import Integration from "./components/organisms/Integration";
+import Profile from "./components/organisms/Profile";
+import Password from "./components/organisms/Password";
+import Preference from "./components/organisms/Preference";
+import Integration from "./components/organisms/Integration";
 import Help from "./components/pages/Help";
+import "./App";
+import './App.scss';
 
 export default class App extends Component {
 	
@@ -47,10 +46,10 @@ export default class App extends Component {
 					<Route name="courses" path="/courses" component={Courses}/>
 					<Route name="search" path="/search" component={Search}/>
 					<Route name="settings" path="/settings" component={Settings}/>
-						<Route name="profile" path="settings/profile" component={Profile}/>
-						<Route name="password" path="settings/password" component={Password}/>
-						<Route name="preferences" path="settings/preferences" component={Preference}/>
-						<Route name="integrations" path="settings/integrations" component={Integration}/>
+						{/* <Route name="profile" path="settings/profile" component={Profile}/> */}
+						{/* <Route name="password" path="settings/password" component={Password}/> */}
+						{/* <Route name="preferences" path="settings/preferences" component={Preference}/> */}
+						{/* <Route name="integrations" path="settings/integrations" component={Integration}/> */}
 					<Route name="help" path="/help" component={Help}/>
 				</Switch>
 			)

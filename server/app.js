e98e5ee84eaaa6de0e7158d8899b9538023f0579
Richard Antao <richardmantao@gmail.com>
2019-10-01 19:15:50 -0400

    Commented out cookie-parser

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const logger = require("morgan");
 const cors = require("cors");
 // const session = require("express-session");
 const uuid = require("uuid");
-const cookieParser = require("cookie-parser");
+// const cookieParser = require("cookie-parser");
 // const dotenv = require('dotenv').config();
 
 // Configurations
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger("dev"));
 app.use(cors());
-app.use(cookieParser());
+// app.use(cookieParser());
 // app.use(session({secret: "shhh"}));
 
 // Routes middleware

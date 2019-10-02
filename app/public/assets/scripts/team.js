f02c30ecd0563aaf8b94fd5689a48678f39399b0
Richard Antao <richardmantao@gmail.com>
2019-10-01 20:11:08 -0400

    Updated team script

--- app/public/assets/scripts/team.js
+++ app/public/assets/scripts/team.js
@@ -1,10 +1,11 @@
 var acc = document.getElementsByClassName("accordion");
 
 for (let i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", () => {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
+
+    if(panel.style.maxHeight){
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";

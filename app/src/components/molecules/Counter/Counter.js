0971d211ff5d4ff24fe450f3d400128a6176b601
Richard Antao <richardmantao@gmail.com>
2019-10-06 13:36:36 -0400

    Updated Counter

--- app/src/components/molecules/Counter/Counter.js
+++ app/src/components/molecules/Counter/Counter.js
@@ -1,41 +1,51 @@
 import React, { Component } from "react";
 import "./Counter.scss";
 
-export default class Counter extends Component{
+export default class Counter extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			isLoading: true
+			isLoading: true,
+			amount: []
 		}
 	}
 	
 	componentDidMount() {
 		this.setState({
-			isLoading: false
+			isLoading: false,
+			amount: []
 		});
 	}
 
 	render() {
-		return(
-			<figure className="counter">
-				<figcaption></figcaption>
-				<img className="" src="" alt=""/>
-				<svg width="100" height="100">
-					<circle className="" cx="50" cy="50" r="50"/>
-			<foreignObject width="100" height="100">
-				<div className="counter-index">
-					<h6>{this.props.count}</h6>
-				</div>
-			</foreignObject>
-			<foreignObject width="100" height="100">
-				<div className="counter-type">
-					<h6>{this.props.type}</h6>
-				</div>
-			</foreignObject>
-				</svg>
-			</figure>
-		)
+		let { amount } = this.state;
+
+		if(amount == 1) {
+			return(
+				<figure>
+
+				</figure>
+			)
+		} else {
+			return(
+				<figure className="counter">
+					<svg width="100" height="100">
+						<circle className="" cx="50" cy="50" r="50" stroke="black" stroke-width="3"/>
+				<foreignObject width="100" height="100">
+					<div className="counter-index">
+						<h6>{this.props.count}</h6>
+					</div>
+				</foreignObject>
+				<foreignObject width="100" height="100">
+					<div className="counter-type">
+						<h6>{this.props.type}</h6>
+					</div>
+				</foreignObject>
+					</svg>
+				</figure>
+			)
+		}
 	}	
 }
 

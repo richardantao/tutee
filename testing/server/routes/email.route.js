5ea4768bca39bbea7d4904f14e40db381a5e1eeb
Richard Antao <richardmantao@gmail.com>
2019-10-05 23:36:48 -0400

    Restructured environments with branches

--- testing/server/routes/email.route.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const controller = require("../controllers/email.controller");
-
-router.post("/beta", controller.beta);
-
-router.post("/contact", controller.contact);
-
-module.exports = router;

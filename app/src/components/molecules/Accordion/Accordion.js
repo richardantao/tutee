34e4346ce1c2425cdf1ffefa9a8e160fa31d9834
Richard Antao <richardmantao@gmail.com>
2019-10-06 23:16:35 -0400

    Renamed component

--- app/src/components/molecules/Accordion/Accordion.js
+++ app/src/components/molecules/Accordion/Accordion.js
@@ -1,32 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Accordion.scss";
 
-export default class Accordion extends Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            isLoading: true
-        }
-    }
-
-    componentDidMount() {
-        this.setState({
-            isLoading: false
-        });
-    }
-
-    render() {
-        let { isLoading } = this.state;
-
-        if(isLoading) {
-            return null;
-        } else {
-            return(
-                <div>
-                    
-                </div>
-            )
-        }   
-    }
+const Accordion = (props) => {
+    return(
+        <div>
+            
+        </div>
+    )
 }
+
+export default Accordion;

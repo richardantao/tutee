0c02b2a4f8c5769c9fd60f36309b235ca9d19b14
Richard Antao <richardmantao@gmail.com>
2019-10-13 23:11:15 -0400

    Wrapped App in redux/Provider

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
 import ReactGA from 'react-ga';
 
 import Dashboard from "./components/pages/Dashboard";
@@ -40,16 +41,18 @@ export default class App extends Component {
 			return null;
 		} else {
 			return (
-				<Switch>
-					<Route name="dashboard" path="/dashboard" component={Dashboard}/>
-					<Route name="calendar" path="/calendar" component={Calendar}/>
-					<Route name="tasks" path="/tasks" component={Tasks}/>
-					<Route name="evaluations" path="/evaluations" component={Evaluations}/>
-					<Route name="courses" path="/courses" component={Courses}/>
-					<Route name="search" path="/search" component={Search}/>
-					<Route name="settings" path="/settings" component={Settings}/>
-					<Route name="help" path="/help" component={Help}/>
-				</Switch>
+				// <Provider>
+					<Switch>
+						<Route name="dashboard" path="/dashboard" component={Dashboard}/>
+						<Route name="calendar" path="/calendar" component={Calendar}/>
+						<Route name="tasks" path="/tasks" component={Tasks}/>
+						<Route name="evaluations" path="/evaluations" component={Evaluations}/>
+						<Route name="courses" path="/courses" component={Courses}/>
+						<Route name="search" path="/search" component={Search}/>
+						<Route name="settings" path="/settings" component={Settings}/>
+						<Route name="help" path="/help" component={Help}/>
+					</Switch>
+				// </Provider>
 			)
 		}	
 	}

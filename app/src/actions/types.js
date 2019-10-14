2d49f6eb01fca31f19007e0ec98bfebe04249578
Richard Antao <richardmantao@gmail.com>
2019-10-14 18:57:42 -0400

    Added action types

--- app/src/actions/types.js
+++ app/src/actions/types.js
@@ -1,2 +1,49 @@
+// years
+export const FETCH_YEARS = "FETCH_YEARS";
+export const EDIT_YEAR = "EDIT_YEAR";
+export const CREATE_YEAR = "CREATE_YEAR";
+export const UPDATE_YEAR = "UPDATE_YEAR";
+export const DELETE_YEAR = "DELETE_YEAR";
+
+// terms
+export const FETCH_TERMS = "FETCH_TERMS";
+export const EDIT_TERM = "EDIT_TERM";
+export const CREATE_TERM = "CREATE_TERM";
+export const UPDATE_TERM = "UPDATE_TERM";
+export const DELETE_TERM = "DELETE_TERM";
+
+// courses
+export const FETCH_COURSES = "FETCH_COURSES";
+export const EDIT_COURSE = "EDIT_COURSE";
+export const CREATE_COURSE = "CREATE_COURSE";
+export const UPDATE_COURSE = "UPDATE_COURSE";
+export const DELETE_COURSE = "DELETE_COURSE";
+
+// modules
+export const FETCH_MODULES = "FETCH_MODULES";
+export const EDIT_MODULE = "EDIT_MODULE";
+export const CREATE_MODULE = "CREATE_MODULE";
+export const UPDATE_MODULE = "UPDATE_MODULE";
+export const DELETE_MODULE = "DELETE_MODULE";
+
+// classes
+export const FETCH_CLASSES = "FETCH_CLASSES";
+export const EDIT_CLASS = "EDIT_CLASS";
+export const CREATE_CLASS = "CREATE_CLASS";
+export const UPDATE_CLASS = "UPDATE_CLASS";
+export const DELETE_CLASS = "DELETE_CLASS";
+
+// evaluations
+export const FETCH_EVALUATIONS = "FETCH_EVALUATIONS";
+export const EDIT_EVALUATION = "EDIT_EVALUATION";
+export const CREATE_EVALUATION = "CREATE_EVALUATION";
+export const UPDATE_EVALUATION = "UPDATE_EVALUATION";
+export const DELETE_EVALUATION = "DELETE_EVALUATION";
+
+// tasks
 export const FETCH_TASKS = "FETCH_TASKS";
-export const NEW_TASK = "NEW_TASK";
+export const EDIT_TASK = "EDIT_TASK";
+export const CREATE_TASK = "CREATE_TASK";
+export const UPDATE_TASK = "UPDATE_TASK";
+export const DELETE_TASK = "DELETE_TASK";
+

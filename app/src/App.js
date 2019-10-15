e9546c643386a3c070cc6a97f279732d1de18070
Richard Antao <richardmantao@gmail.com>
2019-10-15 00:48:28 -0400

    Updated redux

--- app/src/App.js
+++ app/src/App.js
@@ -17,45 +17,25 @@ import "./App";
 import './App.scss';
 
 export default class App extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			isLoading: true
-		}
-	}
-
-	componentDidMount() {
-		this.setState({
-			isLoading: false
-		});
-	}
-
 	initializeReactGA() {
 		ReactGA.initialize('UA-000000-01'); // check tracking id
 		ReactGA.pageview(window.location.pathname + window.location.search);
 	}
 
 	render() {
-		let { isLoading } = this.state;
-
-		if(isLoading) {
-			return null;
-		} else {
-			return (
-				<Provider store={store}>
-					<Switch>
-						<Route name="dashboard" path="/dashboard" component={Dashboard}/>
-						<Route name="calendar" path="/calendar" component={Calendar}/>
-						<Route name="tasks" path="/tasks" component={Tasks}/>
-						<Route name="evaluations" path="/evaluations" component={Evaluations}/>
-						<Route name="courses" path="/courses" component={Courses}/>
-						<Route name="search" path="/search" component={Search}/>
-						<Route name="settings" path="/settings" component={Settings}/>
-						<Route name="help" path="/help" component={Help}/>
-					</Switch>
-				</Provider>
-			)
-		}	
+		return (
+			<Provider store={store}>
+				<Switch>
+					<Route name="dashboard" path="/dashboard" component={Dashboard}/>
+					<Route name="calendar" path="/calendar" component={Calendar}/>
+					<Route name="tasks" path="/tasks" component={Tasks}/>
+					<Route name="evaluations" path="/evaluations" component={Evaluations}/>
+					<Route name="courses" path="/courses" component={Courses}/>
+					<Route name="search" path="/search" component={Search}/>
+					<Route name="settings" path="/settings" component={Settings}/>
+					<Route name="help" path="/help" component={Help}/>
+				</Switch>
+			</Provider>
+		)
 	}
 }

f9d20bf07034ff6e5ef9bb61ea29dbffd0df5fff
Richard Antao <richardmantao@gmail.com>
2019-10-16 12:54:50 -0400

    Added email routes

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,10 +1,15 @@
 const router = require("express").Router();
 const controller = require("../controllers/auth.controller");
 
-router.post("/signup", controller.signup);
+router.post("/contact", controller.contact);
+
+router.post("/invite", )
+
+router.post("/register", controller.register);
 
 router.post("/signin", controller.signin);
 
-router.post("/signout", controller.signout);
+router.delete("/signout", controller.signout);
+
 
 module.exports = router;

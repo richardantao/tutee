bb8f478be633adc5a2405a9b2981c9754fdd8be3
Richard Antao <richardmantao@gmail.com>
2019-10-18 13:53:32 -0400

    Added custom middleware

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const secret = process.env.AUTH_SECRET;
+
+function auth(req, res, next) {
+    const token = req.header("x-auth-token");
+    
+    if(!token) {
+        return res.status(401).json({
+            success: false,
+            message: "No token, authorization denied"
+        });
+    } else {
+        // pass secret here
+        const decoded = jwt.verify(token, null);
+
+        req.user = decoded;
+        next();
+    }
+}
+
+module.exports = auth;

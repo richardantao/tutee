db10ab2ccb77ab59e2c759f351f350efe8f1e83b
Richard Antao <richardmantao@gmail.com>
2019-10-19 18:20:46 -0400

    Added auth action

---
+++ client/src/actions/auth.action.js
@@ -0,0 +1,48 @@
+import { 
+    AUTH_ERROR, USER_LOADING, USER_LOADED, LOGIN_SUCCESS,
+    LOGIN_FAILED, LOGOUT_SUCCESS, REGISTER_SUCCESS, REGISTER_FAILED 
+} from "./types";
+import { returnErrors } from "./errors.action";
+import axios from "axios";
+
+// check and load user
+export const loadUser = () => (dispatch, getState) => {
+    // User loading
+    dispatch({ types: USER_LOADING });
+
+    axios.get("/", tokenConfig(getState))
+    .then(res => {
+        dispatch({
+            type: USER_LOADED,
+            payload: res.data
+        })
+    })
+    .catch(err => {
+        dispatch(returnErrors(err.res.data, err.res.status));
+        dispatch({
+            type: AUTH_ERROR
+        });
+    });
+}
+
+export const tokenConfig = getState => {
+    const token = getState().auth.token;
+
+    // Headers
+    const config = {
+        headers: {
+            "Content-type": "application/json" 
+        }
+    }
+
+    // If token, add to headers
+    if(token) {
+        config.headers["x-auth-token"] = token;
+    }
+
+    return config;
+}
+
+
+
+

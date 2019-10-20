fa0279b7333ecf6827405f37c5818befdf8afe55
Richard Antao <richardmantao@gmail.com>
2019-10-20 18:43:49 -0400

    Changed component name

---
+++ client/src/components/pages/NotFound/NotFound.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+import { connect } from "react-redux";
+
+import LoadingColumn from "../../molecules/LoadingColumn";
+import "./NotFound.scss";
+
+class NotFound extends Component {
+    componentDidMount() {
+    
+    }
+    
+    componentWillUnmount() {
+        
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default connect(null, {})(NotFound);

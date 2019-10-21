108960681cbbbd3fa0e398609bcc95a51b500025
Richard Antao <richardmantao@gmail.com>
2019-10-21 16:22:22 -0400

    Added redux connect

--- client/src/components/organisms/Nav/Nav.js
+++ client/src/components/organisms/Nav/Nav.js
@@ -1,4 +1,8 @@
 import React, { Component, Fragment }from "react";
+
+import {  } from "../../../actions";
+import { connect } from "react-redux";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
 	faTachometerAlt, faCalendarAlt, faTasks, faGraduationCap,
@@ -8,58 +12,52 @@ import { Button } from "react-bootstrap";
 import "./Nav.scss";
 import Logo from "../../atoms/Logo";
 
-export default class Nav extends Component {
-	constructor(props) {
-		super(props);
-	
-		this.state = {
-			isLoading: true
-		}
+class Nav extends Component {
+	state = {
+
 	}
 	
 	componentDidMount() {
-		this.setState({
-			isLoading: false
-		});
+
 	}
 	
 	render() {
-		let { isLoading } = this.state;
-
-		if(isLoading) {
-			return null;
-		} else {
-			return ( 
-				<Fragment>
-					<Logo/> 
-					<nav role="navigation">
-						<Button href="/dashboard">
-							<FontAwesomeIcon icon={faTachometerAlt}/>
-						</Button>
-						<Button href="/calendar">
-							<FontAwesomeIcon icon={faCalendarAlt}/>
-						</Button>
-						<Button href="/tasks">
-							<FontAwesomeIcon icon={faTasks}/>
-						</Button>
-						<Button href="/evaluations" className="adjust">
-							<FontAwesomeIcon icon={faGraduationCap}/>
-						</Button>
-						<Button href="/courses">
-							<FontAwesomeIcon icon={faUniversity}/>
-						</Button>
-						<Button href="/search">
-							<FontAwesomeIcon icon={faSearch}/>
-						</Button>
-						<Button href="/settings" className="secondary-nav-button">
-							<FontAwesomeIcon icon={faCog}/>
-						</Button>
-						<Button href="/help" className="secondary-nav-button">
-							<FontAwesomeIcon icon={faQuestionCircle}/>
-						</Button>
-					</nav>
-				</Fragment>
-			)
-		}
-	}	
+		return ( 
+			<Fragment>
+				<Logo/> 
+				<nav role="navigation">
+					<Button href="/dashboard">
+						<FontAwesomeIcon icon={faTachometerAlt}/>
+					</Button>
+					<Button href="/calendar">
+						<FontAwesomeIcon icon={faCalendarAlt}/>
+					</Button>
+					<Button href="/tasks">
+						<FontAwesomeIcon icon={faTasks}/>
+					</Button>
+					<Button href="/evaluations" className="adjust">
+						<FontAwesomeIcon icon={faGraduationCap}/>
+					</Button>
+					<Button href="/courses">
+						<FontAwesomeIcon icon={faUniversity}/>
+					</Button>
+					<Button href="/search">
+						<FontAwesomeIcon icon={faSearch}/>
+					</Button>
+					<Button href="/settings" className="secondary-nav-button">
+						<FontAwesomeIcon icon={faCog}/>
+					</Button>
+					<Button href="/help" className="secondary-nav-button">
+						<FontAwesomeIcon icon={faQuestionCircle}/>
+					</Button>
+				</nav>
+			</Fragment>
+		)
+	}
 }
+
+const mapStateToProps = state => {
+
+}
+
+export default connect(mapStateToProps, { })(Nav);

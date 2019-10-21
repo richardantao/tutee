adf3438f7a6d4fa846a8aee2dea44c54e9b50cd6
Richard Antao <richardmantao@gmail.com>
2019-10-21 16:20:35 -0400

    Moved component to new folder

---
+++ client/src/components/atoms/SignoutButton/SignoutButton.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+import {  } from "../../../actions/auth.action";
+import { connect } from "react-redux"; 
+
+import "./SignoutButton.scss";
+
+class SignoutButton extends Component {
+    state = {
+
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    
+}
+
+export default connect(mapStateToProps, {  })(SignoutButton);
+

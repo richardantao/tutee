8c64316fe55d0e162c4d7a8bf0aafc70a1fc6048
Richard Antao <richardmantao@gmail.com>
2019-10-21 21:15:06 -0400

    Added auth routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import { Provider } from "react-redux";
 import store from "./store";
 import { loadUser } from "./actions/auth.action";
 
+import Register from "./components/pages/Register";
+import Login from "./components/pages/Login";
 import Dashboard from "./components/pages/Dashboard";
 import Calendar from "./components/pages/Calendar";
 import Tasks from "./components/pages/Tasks";
@@ -14,6 +16,8 @@ import Courses from "./components/pages/Courses";
 import Search from "./components/pages/Search";
 import Settings from "./components/pages/Settings";
 import Help from "./components/pages/Help";
+import Errors from "./components/pages/Errors";
+
 import "./App";
 import './App.scss';
 
@@ -31,6 +35,8 @@ export default class App extends Component {
 		return (
 			<Provider store={store}>
 				<Switch>
+					<Route name="register" path="/register" component={Register}/>
+					<Route name="login" path="/signin" component={Login}/>
 					<Route name="dashboard" path="/dashboard" component={Dashboard}/>
 					<Route name="calendar" path="/calendar" component={Calendar}/>
 					<Route name="tasks" path="/tasks" component={Tasks}/>
@@ -39,8 +45,9 @@ export default class App extends Component {
 					<Route name="search" path="/search" component={Search}/>
 					<Route name="settings" path="/settings" component={Settings}/>
 					<Route name="help" path="/help" component={Help}/>
+					<Route name="errors" path="/errors" component={Errors}/>
 				</Switch>
 			</Provider>
-		)
-	}
-}
+		);
+	};
+};

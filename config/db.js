5a7e88a07289249030d8b44be76dfe32f90bec84
Richard Antao <richardmantao@gmail.com>
2019-10-30 00:21:35 -0400

    Imported new dev db

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-const mongoDB = "mongodb://localhost/TuteeDev";
+const mongoDB = "mongodb://localhost/tutee-dev";
 
 mongoose.connect(mongoDB, {
   useCreateIndex: true,

6077e52371d2e9605b3c303e2e0b44f0a341ecdf
Richard Antao <richardmantao@gmail.com>
2019-10-31 21:24:28 -0400

    Updated reducer

--- client/src/reducers/molecules/counter.reducer.js
+++ client/src/reducers/molecules/counter.reducer.js
@@ -1,14 +1,15 @@
 import { COUNT_DATA } from "../../actions/types";
 
 const initialState = {
-
+    count: 0
 };
 
 export default (state = initialState, action) => {
     switch(action.type) {
         case COUNT_DATA:
             return {
-                
+                ...state,
+                count: action.payload
             };
         default:
             return state;

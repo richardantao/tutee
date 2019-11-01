a1022f04a021af3dc21196259a1e8f5039d381b0
Richard Antao <richardmantao@gmail.com>
2019-10-31 21:24:39 -0400

    Updated action

--- client/src/actions/molecules/counter.action.js
+++ client/src/actions/molecules/counter.action.js
@@ -1,5 +1,14 @@
 import { COUNT_DATA } from "../types";
+import { returnErrors } from "../auth/errors.action";
+import axios from "axios";
 
-const countData = () => {
-    
+export const countData = () => dispatch => {
+    axios.get("/dashboard")
+    .then(res => dispatch({
+        type: COUNT_DATA,
+        payload: res.data
+    }))
+    .catch(err => dispatch(
+        returnErrors(err.data, err.status)
+    ));    
 };

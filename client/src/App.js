29c15d28a09e4607d8e11264cb2d67ef4a510a78
Richard Antao <richardmantao@gmail.com>
2019-11-03 19:03:49 -0500

    Substituted in new academics component

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,9 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 import Calendar from "./components/calendar/Calendar";
+import Academics from "./components/academics/Academics";
 import Tasks from "./components/tasks/Tasks";
 import Evaluations from "./components/evaluations/Evaluations";
-import Courses from "./components/courses/Courses";
 import Search from "./components/search/Search";
 import Settings from "./components/settings/Settings";
 import Help from "./components/help/Help";
@@ -33,9 +33,9 @@ export default class App extends Component {
 					<Route name="login" path="/signin" component={Login}/>
 					<Route name="dashboard" path="/dashboard" component={Dashboard}/>
 					<Route name="calendar" path="/calendar" component={Calendar}/>
+					<Route name="academics" path="/academics" component={Academics}/>
 					<Route name="tasks" path="/tasks" component={Tasks}/>
 					<Route name="evaluations" path="/evaluations" component={Evaluations}/>
-					<Route name="courses" path="/courses" component={Courses}/>
 					<Route name="search" path="/search" component={Search}/>
 					<Route name="settings" path="/settings" component={Settings}/>
 					<Route name="help" path="/help" component={Help}/>

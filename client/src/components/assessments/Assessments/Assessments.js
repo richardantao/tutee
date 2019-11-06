de31ce0a3dfad14c647f84c3b9cb1543c55e9d51
Richard Antao <richardmantao@gmail.com>
2019-11-06 12:15:03 -0500

    Stripped component

--- client/src/components/assessments/Assessments/Assessments.js
+++ client/src/components/assessments/Assessments/Assessments.js
@@ -1,5 +1,4 @@
-import React, { Component, Fragment } from "react";
-import { Helmet } from "react-helmet";
+import React, { Component } from "react";
 
 import { connect } from "react-redux";
 import { fetchAssessments, fetchPastAssessments } from "../../../actions/data/assessments.action";
@@ -7,20 +6,13 @@ import PropTypes from "prop-types";
 
 import { Button, Col, Row } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faEdit } from "@fortawesome/free-solid-svg-icons";
-
-import Nav from "../../global/Nav";
-import Header from "../../global/Header";
-import AssEditModal from "../AssEditModal";
-import AssNewModal from "../AssNewModal";
-import Select from "../../global/Select";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
 
 import "./Assessments.scss";
 
 class Assessments extends Component{
 	state = {
-		editModal: false,
-		newModal: false
+		
 	};
 
 	static propTypes = {
@@ -35,20 +27,7 @@ class Assessments extends Component{
 		this.props.fetchAssessments();
 	};
 
-	editAssessmentModal = () => {
-		this.setState({
-			editModal: true
-		});
-	};
-
-	newAssessmentModal = () => {
-		this.setState({
-			newModal: true
-		});
-	};
-
 	render() {
-		const { editModal, newModal } = this.state;
 		const { assessments } = this.props.assessment;
 
 		const assessmentRecords = assessments.map(({ _id, title, course, date, time, location}) => (
@@ -69,43 +48,9 @@ class Assessments extends Component{
 		));
 		 
 		return (
-			<Fragment>
-				<Helmet>
-					<title>My Tutee | Assessments</title>
-				</Helmet>
-				<Nav/>
-				<div id="assessments">
-					<Row className="header">
-						<Col>
-							<Header header="Assessments"/> 
-						</Col>
-						<Col>
-							<Button onClick={this.newAssessmentModal}><FontAwesomeIcon icon={faPlus}/> New Assessment</Button>
-						</Col>
-					</Row>
-					<Row className="body ass-body">
-						<Col>
-							<Select placeholder="Filter by Course.."/>
-						</Col>
-						<Col>
-							<Button onClick={this.fetchAssessments} className="current">Current</Button>
-							<Button onClick={this.fetchPastAssessments} className="past">Past</Button>
-						</Col>	
-					</Row>
-					<Row>
-						<Col>
-							{assessmentRecords}
-						</Col>
-					</Row>
-
-					{ editModal ? (
-						<AssEditModal className="modal"/>
-					): null }
-					{ newModal ? (
-						<AssNewModal className="modal"/>
-					): null }
-				</div>
-			</Fragment>
+			<Col className="assessments-column">
+				{assessmentRecords}
+			</Col>
 		);	
 	};
 };
